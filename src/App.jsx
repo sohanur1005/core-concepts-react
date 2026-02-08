70ec2d91f66e72d58ea483ce112fa5182a5db774
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-08 19:29:25 +0600

    Send this

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,53 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './Todo'
 
 function App() {
 
 
   return (
     <>
-      
+
       <h1>Vite + React</h1>
-      <Device name="Laptop" price="50000"></Device>
+      <Todo task="Learn React"
+      isDone={true}></Todo>
+      <Todo task="Explore core concepts" 
+      isDone={true}></Todo>
+      <Todo task="Try jsx"
+      isDone={true}></Todo>
+      {/* <Device name="Laptop" price="50000"></Device>
       <Device name="Mobile" price="20000"></Device>
       <Device name="Watch" price="5000"></Device>
       <Person></Person>
       <Student grade="7" score="99"></Student>
       <Student></Student>
-      <Developer></Developer>
+      <Developer></Developer> */}
     </>
   )
 }
 
-function Device(props){
+function Device(props) {
   console.log(props)
   return <h2>This device: {props.name} Price: {props.price} </h2>
 }
 
 
-function Person(props){
+function Person(props) {
   console.log(props)
-  const age=25;
-  const person={name:'Sohanur',age:'12'}
-  return(
-   <h3>I am {person.name} with age: {age}</h3>
+  const age = 25;
+  const person = { name: 'Sohanur', age: '12' }
+  return (
+    <h3>I am {person.name} with age: {age}</h3>
   )
 }
 
 // Destructuring
-const {grade,score}={grade:"7",score:"99"}
-function Student({grade,score}){
-  console.log(grade,score)
+const { grade, score } = { grade: "7", score: "99" }
+function Student({ grade, score }) {
+  console.log(grade, score)
   return (
-    
+
     <div className='student'>
       <h3>I am a student</h3>
       <p>Score: {score}</p>
@@ -50,14 +57,14 @@ function Student({grade,score}){
   )
 }
 
-function Developer(){
-  const developerStyle={
-    border:'2px solid purple',
-    margin:'20px',
-    padding:'20px',
-    borderRadius:'20px'
+function Developer() {
+  const developerStyle = {
+    border: '2px solid purple',
+    margin: '20px',
+    padding: '20px',
+    borderRadius: '20px'
   }
-  return(
+  return (
     <div style={developerStyle}>
       <h5>Devo Devo</h5>
       <p>Coding</p>
---
+++ src/Todo.jsx
@@ -0,0 +1,34 @@
+
+/*export default function Todo({task}){
+    return (
+        <li>Task: {task}</li>
+    )
+}*/
+
+
+
+ // conditional rendering option 1
+// export default function Todo({task,isDone}){
+//   if(isDone===true)
+//   {
+//     return <li>Finished: {task}</li>
+//   }
+//   else{
+//     return <li>Unfinished: {task}</li>
+//   }
+// }
+
+// conditional rendering option 2
+// export default function Todo({task,isDone}){
+//   if(isDone)
+//   {
+//     return <li>Finished: {task}</li>
+//   }
+
+//     return <li>Unfinished: {task}</li>
+  
+// }
+
+
+// conditional rendering option 3
+
